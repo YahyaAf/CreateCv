142bdb114009280139d2ac3322a28e902e1de153
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-11-09 20:41:44 +0100

    create stepper pour la formulaire

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let form1 = document.getElementById('form1')
+let form2 = document.getElementById('form2')
+let form3 = document.getElementById('form3')
+let form4 = document.getElementById('form4')
+let form5 = document.getElementById('form5')
+let form6 = document.getElementById('form6')
+let form7 = document.getElementById('form7')
+
