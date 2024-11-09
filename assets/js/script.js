60c9cd334e68a116a1242fdf250f760d8d45107a
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-11-09 21:31:46 +0100

    create function next et back pour switch forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
-let form1 = document.getElementById('form1')
-let form2 = document.getElementById('form2')
-let form3 = document.getElementById('form3')
-let form4 = document.getElementById('form4')
-let form5 = document.getElementById('form5')
-let form6 = document.getElementById('form6')
-let form7 = document.getElementById('form7')
+const forms = [
+    document.getElementById('form1'),
+    document.getElementById('form2'),
+    document.getElementById('form3'),
+    document.getElementById('form4'),
+    document.getElementById('form5'),
+    document.getElementById('form6'),
+    document.getElementById('form7')
+];
+let counterForm = 0;
 
+const Next = () => {
+    if (counterForm < forms.length - 1) {  
+        forms[counterForm].classList.add('hidden');  
+        counterForm++;  
+        forms[counterForm].classList.remove('hidden');
+    }
+};
+
+const Back = () => {
+    if (counterForm > 0) {  
+        forms[counterForm].classList.add('hidden'); 
+        counterForm--;  
+        forms[counterForm].classList.remove('hidden');
+    }
+};
