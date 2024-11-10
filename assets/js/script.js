dbc601af5a8b998a6e97e77c342414dc389e83a2
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-11-10 17:10:43 +0100

    ajouter dynamique form dans la partie saisie un langagues

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,51 @@ const Back = () => {
         steppers[counterForm].classList.remove('bg-gray-300','text-gray-900');
     }
 };
+
+let formLangagueCounter = 2;
+const AddFormLangague = (event) => {
+    event.preventDefault();
+    
+    const formContainer = document.createElement("div");
+    formContainer.className = "w-full lg:w-12/12 px-4";
+
+    formContainer.innerHTML=`
+        <hr class="border-t-2 border-gray-300 my-4">
+
+        <div class="w-full lg:w-6/12 px-4">
+                <div class="relative w-full mb-3">
+                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">
+                                Langue
+                        </label>
+                        <input id="lang${formLangagueCounter}" type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Votre Compétence">
+                </div>
+        </div>
+        <div class="w-full lg:w-12/12 px-4">
+                <div class="relative w-full mb-3">
+                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="language-level">
+                                Niveau de langue
+                        </label>
+                        <select id="niv${formLangagueCounter}" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
+                                <option value="A1">A1 - Débutant</option>
+                                <option value="A2">A2 - Élémentaire</option>
+                                <option value="B1">B1 - Intermédiaire</option>
+                                <option value="B2">B2 - Intermédiaire avancé</option>
+                                <option value="C1">C1 - Avancé</option>
+                                <option value="C2">C2 - Maîtrise</option>
+                        </select>
+                </div>
+        </div>
+        <button type="button" onclick="DeleteFormLangague(this)" class="bg-red-500 text-white py-1 px-1 rounded-md shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400">
+            Remove
+        </button>
+    `;
+
+    document.getElementById("parent-lang").appendChild(formContainer);
+    formLangagueCounter++;
+
+}
+
+const DeleteFormLangague = (button)  => {
+    const formContainer = button.parentElement;
+    formContainer.remove();
+}
